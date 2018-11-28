5a09f6199b27bbf974458589c85708103738026b
Don Nguyen <don@aerofiler.com>
2018-11-28 14:55:28 +1100

    Styles

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const myStore = createStore(reducers)
 const App = () => {
   return (
     <Provider store={myStore}>
-      <View>
+      <View style={{ flex: 1}}>
         <Header headerText="Tech Stack" />
          <LibraryList />
       </View>
