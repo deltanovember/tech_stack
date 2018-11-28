4951bfa018ec149f9d718059412dd36168a5591a
Don Nguyen <don@aerofiler.com>
2018-11-28 14:33:56 +1100

    Show titles

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,35 +1,18 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
-import { connect } from 'react-redux';
+import { Text } from 'react-native';
 import { CardSection } from './common';
-import * as actions from '../actions';
 
 class ListItem extends Component {
    render() {
-     const { titleStyle } = styles;
-     const { id, title } = this.props.library;
-     console.log(this.props);
+
      return (
-       <TouchableWithoutFeedback
-        onPress={() => this.props.selectLibrary(id)}
-       >
-        <View>
-           <CardSection>
-             <Text style={titleStyle}>
-               {this.props.library.title}
-             </Text>
-           </CardSection>
-         </View>
-       </TouchableWithoutFeedback>
+       <CardSection>
+         <Text>
+           {this.props.library.item.title}
+         </Text>
+       </CardSection>
      );
    }
  }
 
- const styles = {
-   titleStyle: {
-     fontSize: 18,
-     paddingLeft: 15
-   }
- };
-
- export default connect(null, actions)(ListItem);
+export default ListItem;
