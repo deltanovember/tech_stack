fe4ea47a02e9cedbc2dc8508f3a4580e8b09d53f
Don Nguyen <don@aerofiler.com>
2018-12-02 19:12:26 +1100

    Add spring

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, LayoutAnimation } from 'react-native';
 import { CardSection } from './common';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class ListItem extends Component {
 
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
   renderDescription() {
     const { library, expanded } = this.props;
     if (expanded) {
       return (
-        <Text>{library.item.description}</Text>
+        <CardSection>
+          <Text style={{ flex: 1 }}>{library.item.description}</Text>
+        </CardSection>
       );
     }
   }
